a7a4944408e611503b6da18ca57b4a52cf6a244a
Bfacter <bhawanshbaleja@gmail.com>
2023-12-29 00:38:11 +0530

    Overlay modal on profile click added

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,27 +1,35 @@
+import React, { useState } from "react";
 import { ScrollView, Text, TouchableOpacity, View } from "react-native";
-import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import styles from "../styles/home.style";
 import COLORS from "../constants/colors";
 import Main from "../components/home/Main";
 import { useNavigation } from "@react-navigation/native";
 import { Ionicons } from "@expo/vector-icons";
+import ProfileOverlay from "../screens/ProfileOverlay";
 
 const HomeScreen = () => {
   const navigation = useNavigation();
-  const openDrawer = () => {
-    navigation.navigate("DrawerNavigation");
+  const [isProfileOverlayVisible, setProfileOverlayVisible] = useState(false);
+
+  const openProfileOverlay = () => {
+    setProfileOverlayVisible(true);
   };
+
+  const closeProfileOverlay = () => {
+    setProfileOverlayVisible(false);
+  };
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <View style={styles.appBarContainer}>
         <View style={styles.appBar}>
-          <TouchableOpacity onPress={openDrawer}>
+          <TouchableOpacity onPress={openProfileOverlay}>
             <Ionicons name="person-outline" size={24} color={COLORS.primary} />
           </TouchableOpacity>
           <Text style={styles.appBarHeading}>Fanverse</Text>
           <View style={styles.appBarRight}>
-            <TouchableOpacity>
+            <TouchableOpacity onPress={openProfileOverlay}>
               <Ionicons
                 name="ios-notifications-outline"
                 size={24}
@@ -39,6 +47,10 @@ const HomeScreen = () => {
         </View>
       </View>
       <Main />
+
+      {isProfileOverlayVisible && (
+        <ProfileOverlay onClose={closeProfileOverlay} />
+      )}
     </SafeAreaView>
   );
 };
