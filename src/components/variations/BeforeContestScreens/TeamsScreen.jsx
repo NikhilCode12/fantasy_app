6efd67e0ac2b5f73db841f9a9d409e469bd19239
NikhilCode12 <promaxchessace@gmail.com>
2024-02-22 00:21:29 +0530

    add: teamsScreen done

--- src/components/variations/BeforeContestScreens/TeamsScreen.jsx
+++ src/components/variations/BeforeContestScreens/TeamsScreen.jsx
@@ -1,35 +1,74 @@
-import {
-  TouchableOpacity,
-  ScrollView,
-  StyleSheet,
-  Text,
-  View,
-} from "react-native";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
 import React from "react";
 import COLORS from "../../../constants/colors";
 import { Ionicons } from "@expo/vector-icons";
+import UserTeamsCard from "./_components/UserTeamsCard";
+import teamsData from "../../../constants/userteamsdummy.json";
 
 const TeamsScreen = () => {
   const teams = 20;
+  const totalTeams = Math.floor(Math.random() * 10000);
   return (
     <View style={styles.container}>
-      <ScrollView style={{ flex: 1 }}>
+      <ScrollView style={{ flex: 1 }} showsVerticalScrollIndicator={false}>
         {/* Teams Details Container */}
         <View style={styles.detailsContainer}>
-          <View>
-            <Text>Joined</Text>
+          <View
+            style={{
+              width: "50%",
+              justifyContent: "center",
+              gap: 4,
+              borderRadius: 5,
+              padding: 8,
+            }}
+          >
+            <Text style={styles.headingText}>Teams Joined</Text>
             <View
               style={{
                 flexDirection: "row",
                 gap: 5,
                 alignItems: "center",
+                justifyContent: "center",
               }}
             >
               <Text style={styles.teamText}>{teams}</Text>
-              <Ionicons name="shirt" size={14} color={COLORS.silver} />
+              <Ionicons name="shirt-outline" size={14} color={COLORS.primary} />
+            </View>
+          </View>
+          <View
+            style={{
+              width: "50%",
+              justifyContent: "center",
+              gap: 4,
+              backgroundColor: COLORS.bgLightBlack,
+              borderRadius: 5,
+              padding: 8,
+            }}
+          >
+            <Text style={styles.headingText}>Total Teams</Text>
+            <View
+              style={{
+                flexDirection: "row",
+                gap: 5,
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <Text style={styles.teamText}>
+                {totalTeams.toLocaleString("en-IN")}
+              </Text>
+              <Ionicons name="shirt" size={14} color={COLORS.primary} />
             </View>
           </View>
         </View>
+        {/* User and their teams card */}
+        {teamsData.map((user, index) => (
+          <UserTeamsCard
+            key={index}
+            username={user.username}
+            teams={user.teams}
+          />
+        ))}
       </ScrollView>
     </View>
   );
@@ -45,14 +84,25 @@ const styles = StyleSheet.create({
     paddingVertical: 16,
   },
   detailsContainer: {
-    marginTop: 20,
-    backgroundColor: COLORS.transparentBg,
-    padding: 12,
-    borderRadius: 5,
+    marginTop: 4,
+    padding: 4,
+    borderWidth: 1,
+    borderColor: COLORS.lightGray,
+    justifyContent: "space-between",
+    flexDirection: "row",
+    borderRadius: 10,
+    marginBottom: 20,
   },
   teamText: {
-    color: COLORS.light_grey,
+    color: COLORS.light,
+    fontSize: 13,
+    fontWeight: "bold",
+  },
+  headingText: {
+    color: COLORS.silver,
     fontSize: 14,
     fontWeight: "bold",
+    width: "100%",
+    textAlign: "center",
   },
 });
