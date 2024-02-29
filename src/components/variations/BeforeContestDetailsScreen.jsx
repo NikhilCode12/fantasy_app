0c21f25692e4650078eff2ef2975f6bc1187daa4
NikhilCode12 <promaxchessace@gmail.com>
2024-03-01 02:45:11 +0530

    update: stripe payment sheet and integration

--- src/components/variations/BeforeContestDetailsScreen.jsx
+++ src/components/variations/BeforeContestDetailsScreen.jsx
@@ -1,5 +1,5 @@
-import { Text, TouchableOpacity, View, BackHandler } from "react-native";
-import React, { useEffect } from "react";
+import { Text, TouchableOpacity, View, BackHandler, Alert } from "react-native";
+import React, { useEffect, useState } from "react";
 import styles from "../../styles/variations.style.js";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Ionicons } from "@expo/vector-icons";
@@ -11,6 +11,8 @@ import {
   RulesScreen,
   TeamsScreen,
 } from "./BeforeContestScreens/index.js";
+import { useStripe } from "@stripe/stripe-react-native";
+import axios from "axios";
 
 const Tab = createMaterialTopTabNavigator();
 
@@ -29,6 +31,54 @@ const BeforeContestDetailsScreen = ({ route }) => {
 
     return () => backHandler.remove();
   }, []);
+
+  const { initPaymentSheet, presentPaymentSheet } = useStripe();
+
+  const handleJoinContest = async () => {
+    // send request to server to create payment intent
+    try {
+      const response = await axios.post(
+        "https://localhost:5000/api/payments/intents",
+        {
+          amount: Math.floor(1500),
+        }
+      );
+
+      if (response.error) {
+        Alert.alert("Something went wrong!");
+        return;
+      }
+
+      // initialize payment sheet to place order
+      const paymentIntent = response.data.paymentIntent;
+
+      const initResponse = await initPaymentSheet({
+        merchantDisplayName: "Fannverse",
+        paymentIntentClientSecret: paymentIntent,
+      });
+      if (initResponse.error) {
+        Alert.alert("Something went wrong!");
+        return;
+      }
+
+      // present payment sheet from stripe
+      const paymentResponse = await presentPaymentSheet();
+      if (paymentResponse.error) {
+        Alert.alert(
+          `Error code: ${paymentResponse.error.code}`,
+          paymentResponse.error.message
+        );
+        return;
+      }
+
+      // after payment is done, navigate to contest screen
+      navigation.navigate("Contest", {
+        data: data,
+        amount: amount,
+        variation: variation,
+      });
+    } catch (err) {}
+  };
 
   return (
     <SafeAreaView style={styles.container}>
@@ -87,13 +137,8 @@ const BeforeContestDetailsScreen = ({ route }) => {
       {/* Join Contest Button */}
       <TouchableOpacity
         style={styles.joinContestButton}
-        onPress={() => {
-          navigation.navigate("PlayerSelection", {
-            data,
-            amount,
-            variation,
-          });
-        }}
+        onPress={handleJoinContest}
+        // disabled={!loading}
       >
         <Text style={styles.joinContestButtonText}>
           {"Join Contest at \u20B950"}
