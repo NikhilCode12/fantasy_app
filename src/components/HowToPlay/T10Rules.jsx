661212e8f66d8f9fffd26337a505d66a548bb48a
Aryan-Kohli <106698463+Aryan-Kohli@users.noreply.github.com>
2024-03-04 01:53:17 +0530

    made howToPlay for T20

---
+++ src/components/HowToPlay/T10Rules.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Animated,
+  Easing,
+  Button,
+  TextInput,
+  ScrollView,
+  Image,
+} from "react-native";
+import COLORS from "../../constants/colors";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+import { Ionicons } from "@expo/vector-icons";
+import DeleteIcon from "../../components/common/DeleteIcon.js";
+import Toast from "react-native-toast-message";
+export default function T10Rules() {
+  return <View></View>;
+}
