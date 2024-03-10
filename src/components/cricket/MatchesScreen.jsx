11632568e24e4738fd30eb19aedb80c02f9ce063
NikhilCode12 <promaxchessace@gmail.com>
2024-03-10 13:24:22 +0530

    update: match screen

--- src/components/cricket/MatchesScreen.jsx
+++ src/components/cricket/MatchesScreen.jsx
@@ -273,7 +273,7 @@ const MatchesScreen = ({ onMatchCardPress }) => {
           const currentTime = new Date().getTime();
           const matchTime = new Date(match.dateTimeGMT).getTime();
           const timeDifference = matchTime - currentTime;
-          if (timeDifference <= 2 * 60 * 60 * 1000) {
+          if (timeDifference <= 24 * 60 * 60 * 1000 && timeDifference > 0) {
             {
               /* // console.log(index, "  ", match.dateTimeGMT); */
             }
